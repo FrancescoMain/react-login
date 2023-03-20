421cd6231f8c7fbc9018431811e57b1672b639c4
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-20 14:39:28 +0100

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,30 @@ import { css } from "styled-components";
 import { Container } from "./App.styles";
 import Register from "./pages/Auth/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+]);
 
 function App() {
   return (
-    <Container>
-      {/* <Navbar></Navbar> */}
-      {/* <Login></Login> */}
-      <Register></Register>
-    </Container>
+    <>
+      <Navbar></Navbar>
+
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </>
   );
 }
 
