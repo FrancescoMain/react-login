4c49ffcc16189ce61af290a692b8691404675dc0
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-20 14:39:57 +0100

    creati styled component

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,1 +1,30 @@
 import styled from 'styled-components'
+
+export const Nav = styled.nav`
+width: 90%;
+background-color: #bbc5ce;
+margin: 0 auto;
+height: 80px;
+border-radius: 20px
+
+`;
+
+export const UnList = styled.ul`
+display:flex;
+list-style:none;
+justify-content: space-around;
+align-items:center;
+height:100%
+
+
+`;
+
+export const Link = styled.a`
+text-decoration:none;
+color:white;
+
+  &:hover {
+    color: red;
+  }
+`;
+
