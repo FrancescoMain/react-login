ecaed52500847bfb52e5ccb05f340cc61f1d2690
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-21 12:38:32 +0100

    delete react node

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -6,7 +6,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Wrapper } from "../../App.styles";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 import { TextField } from "@mui/material";
 import { Form } from "./style";
 import Button from "@mui/material/Button";
