2f6a4936f9e7ed85590cb1581eae5d5ff3d337e4
francescoMain <cesaranofrancescomain@gmail.com>
2023-03-21 12:33:00 +0100

    aggiunto wrapper

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -5,3 +5,15 @@ display:flex;
 justify-content:center;
 align-items:center;
 height: 100vh;`;
+
+export const Wrapper = styled.div`
+background-color: #bbc5ce;
+min-height: 400px;
+min-width: 600px;
+border-radius: 20px;
+padding:20px;
+box-shadow: 0 8px 40px -12px rgba(0,0,0,0.3);
+&:hover {
+  box-shadow: 0 16px 70px -12.125px rgba(0,0,0,0.3);}
+
+`;
